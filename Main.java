a6bb0119b834e1b8c3869716cc292b3b432d65b3
saumyadayal <saumyaadayall@gmail.com>
2026-03-31 20:38:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         ExpenseManager manager = new ExpenseManager();
 
         while (true) {
-            System.out.println("\n1. Add Expense\n2. View Total\n3. Category Report\n4. Exit");
+            System.out.println("\n==============================");
+            System.out.println("  EXPENSE TRACKER MENU   ");
+            System.out.println("==============================");
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Total");
+            System.out.println("3. Category Report");
+            System.out.println("4. Smart Summary");
+            System.out.println("5. Exit");
+            System.out.println("==============================");
+
+            System.out.print("Enter choice: ");
             int choice = sc.nextInt();
-            sc.nextLine();
+            sc.nextLine(); // consume newline
 
             if (choice == 1) {
                 System.out.print("Amount: ");
@@ -25,16 +36,29 @@ public class Main {
                 String desc = sc.nextLine();
 
                 manager.addExpense(new Expense(amount, category, date, desc));
-            }
+                System.out.println(" Expense added successfully!");
+
+            } 
             else if (choice == 2) {
-                System.out.println("Total Expense: " + manager.getTotalExpense());
-            }
+                System.out.println(" Total Expense: ₹" + manager.getTotalExpense());
+            } 
             else if (choice == 3) {
+                System.out.println(" Category Report:");
                 System.out.println(manager.categoryWiseExpense());
-            }
-            else {
+            } 
+            else if (choice == 4) {
+               
+                ReportGenerator.generateSummary(manager.getExpenses());
+            } 
+            else if (choice == 5) {
+                System.out.println(" Exiting... Thank you!");
                 break;
+            } 
+            else {
+                System.out.println("Invalid choice! Try again.");
             }
         }
+
+        sc.close();
     }
 }
